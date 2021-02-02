a43933878a09649c2225e38256db5978a0f6a0ef
MaKey <MaKeyruan@mumuxili.com>
2021-02-02 18:00:40 +0800

    添加loading

--- add.js
+++ add.js
@@ -14,4 +14,26 @@ function deleteG7(params) {
 
 function deleteG7(params) {
     console.log("删除G6数据源")
-}
+}+!!
+
+
+
+  wx.showLoading({
+        title: "loading...",
+        icon: "none",
+        success: () => {
+          app.globalData.isShowLoading = true ;
+        },
+      });
+      Promise.all(app.globalData.promiseArr).then(res=>{
+        console.log(res)
+        app.globalData.promiseArr = []
+        if (app.globalData.isShowLoading) {
+          wx.hideLoading()
+          app.globalData.isShowLoading = false
+        }
+    }).catch(err=>{
+
+    })
+
+
